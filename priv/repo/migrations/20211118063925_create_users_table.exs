e35ed7b076606fc026c6c4a13d25e5a9acfdd4a1
joaoeliandro <eliandrogermano@gmail.com>
2021-11-19 01:35:41 -0300

    refactor: change code structure

--- priv/repo/migrations/20211118063925_create_users_table.exs
+++ priv/repo/migrations/20211118063925_create_users_table.exs
@@ -2,7 +2,7 @@ defmodule Delivery.Repo.Migrations.CreateUsersTable do
   use Ecto.Migration
 
   def change do
-    create table :users do
+    create table(:users) do
       add :name, :string
       add :cpf, :string
       add :age, :integer
