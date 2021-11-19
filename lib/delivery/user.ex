71345b44685876b5c5496fb84def21ce35f679ef
joaoeliandro <eliandrogermano@gmail.com>
2021-11-19 01:36:43 -0300

    feat(user): add user module as schema and changeset

---
+++ lib/delivery/user.ex
@@ -0,0 +1,42 @@
+defmodule Delivery.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  @required_params [:name, :cpf, :age, :email, :password, :address, :cep]
+
+  schema "users" do
+    field :name, :string
+    field :cpf, :string
+    field :age, :integer
+    field :email, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+    field :address, :string
+    field :cep, :string
+
+    timestamps()
+  end
+
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> validate_length(:password, min: 8)
+    |> validate_length(:cep, is: 8)
+    |> validate_length(:cpf, is: 11)
+    |> validate_number(:age, greater_than_or_equal_to: 18)
+    |> unique_constraint([:cpf])
+    |> unique_constraint([:email])
+    |> put_password_hash()
+  end
+
+  defp put_password_hash(%Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, Pbkdf2.add_hash(password))
+  end
+
+  defp put_password_hash(changeset), do: changeset
+end
