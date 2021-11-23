efbbceb1399e9d84830207609335d625c9616021
joaoeliandro <eliandrogermano@gmail.com>
2021-11-23 20:17:59 -0300

    feat: add facade get user with by_id function

--- lib/delivery.ex
+++ lib/delivery.ex
@@ -1,5 +1,7 @@
 defmodule Delivery do
   alias Delivery.Users.Create, as: UsersCreate
+  alias Delivery.Users.Get, as: UserGet
 
   defdelegate create_user(params), to: UsersCreate, as: :call
+  defdelegate get_user_by_id(id), to: UserGet, as: :by_id
 end
