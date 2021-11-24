4d0815bacd57ab2bcb975cd53403b929a4b33ea2
joaoeliandro <eliandrogermano@gmail.com>
2021-11-23 23:25:33 -0300

    feat: add users delete facade

--- lib/delivery.ex
+++ lib/delivery.ex
@@ -1,7 +1,9 @@
 defmodule Delivery do
   alias Delivery.Users.Create, as: UsersCreate
   alias Delivery.Users.Get, as: UserGet
+  alias Delivery.Users.Delete, as: UserDelete
 
   defdelegate create_user(params), to: UsersCreate, as: :call
   defdelegate get_user_by_id(id), to: UserGet, as: :by_id
+  defdelegate delete_by_id(id), to: UserDelete, as: :call
 end
