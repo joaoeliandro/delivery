185d991fd595ea6e0d2e5202b6bd236e9d989a46
joaoeliandro <eliandrogermano@gmail.com>
2022-03-09 03:09:45 -0300

    feat: added update user on facade fucntion

--- lib/delivery.ex
+++ lib/delivery.ex
@@ -2,8 +2,10 @@ defmodule Delivery do
   alias Delivery.Users.Create, as: UsersCreate
   alias Delivery.Users.Get, as: UserGet
   alias Delivery.Users.Delete, as: UserDelete
+  alias Delivery.Users.Update, as: UserUpdate
 
   defdelegate create_user(params), to: UsersCreate, as: :call
   defdelegate get_user_by_id(id), to: UserGet, as: :by_id
   defdelegate delete_by_id(id), to: UserDelete, as: :call
+  defdelegate update_user(id), to: UserUpdate, as: :call
 end
